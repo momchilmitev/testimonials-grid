5c5d32b0e85e2013caa64d1ed1f5fe961b14bbcf
momchilmitev <momchilmitev87@gmail.com>
2021-08-19 10:28:36 +0200

    adding the router class with basic logic

--- core/Router.php
+++ core/Router.php
@@ -1,3 +1,20 @@
 <?php
 
-echo 'Router Class';
+class Router
+{
+    private $routes = [];
+
+    public function setRoutes(array $routes)
+    {
+        $this->routes = $routes;
+    }
+
+    public function getFilename(string $url)
+    {
+        foreach ($this->routes as $route => $file) {
+            if (strpos($url, $route) !== false) {
+                return $file;
+            }
+        }
+    }
+}
