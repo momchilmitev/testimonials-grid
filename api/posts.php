9b08916a13f2e4ef6eb1637ae2f433b55684dc9c
momchilmitev <momchilmitev87@gmail.com>
2021-08-22 17:30:42 +0200

    adding the fetch all post functionality in posts controller

--- api/posts.php
+++ api/posts.php
@@ -1,3 +1,25 @@
 <?php
 
-echo 'posts api endpoint';
+require(__DIR__ . '/../config.php');
+
+$url = $_SERVER['REQUEST_URI'];
+
+if (stripos($url, '/') !== 0) {
+    $url = "/$url";
+}
+
+$dbInstance = new DB();
+$dbConnection = $dbInstance->connect($db);
+
+if ($_SERVER['REQUEST_URI'] == '/posts' && $_SERVER['REQUEST_METHOD'] == 'GET') {
+    $posts = getAllPosts($dbConnection);
+    echo json_encode($posts);
+}
+
+function getAllPosts($db)
+{
+    $statement = $db->prepare('SELECT * FROM posts');
+    $statement->execute();
+    $result = $statement->setFetchMode(PDO::FETCH_ASSOC);
+    return $statement->fetchAll();
+}
