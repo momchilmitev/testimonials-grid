3dd3f4f399b228d9ae462e64ffb77211a6d978f2
momchilmitev <momchilmitev87@gmail.com>
2021-08-22 19:15:46 +0200

    adding the add post functionality in the post controller

--- api/posts.php
+++ api/posts.php
@@ -16,10 +16,46 @@ if ($_SERVER['REQUEST_URI'] == '/posts' && $_SERVER['REQUEST_METHOD'] == 'GET') 
     echo json_encode($posts);
 }
 
+if ($_SERVER['REQUEST_URI'] == '/posts' && $_SERVER['REQUEST_METHOD'] == 'POST') {
+    $input = $_POST;
+    $postID = addPost($input, $dbConnection);
+
+    if ($postID) {
+        $input['id'] = $postID;
+        $input['link'] = "/posts/$postID";
+    }
+
+    echo json_encode($input);
+}
+
 function getAllPosts($db)
 {
     $statement = $db->prepare('SELECT * FROM posts');
     $statement->execute();
     $result = $statement->setFetchMode(PDO::FETCH_ASSOC);
     return $statement->fetchAll();
+}
+
+function addPost($postData, $db)
+{
+    $sql = "INSERT INTO posts (title, status, content, user_id) VALUES (:title, :status, :content, :user_id)";
+
+    $statement = $db->prepare($sql);
+    bindValues($statement, $postData);
+    $statement->execute();
+
+    return $db->lastInsertId();
+}
+
+function bindValues($statement, $params)
+{
+    $allowedFields = ['title', 'status', 'content', 'user_id'];
+
+    foreach ($params as $param => $value) {
+        if (in_array($param, $allowedFields)) {
+            $statement->bindValue(':' . $param, $value);
+        }
+    }
+
+    return $statement;
 }
